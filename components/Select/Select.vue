59213a8dd58b7c091c44d5d48af7766bc34e3c76
Mihail <maik12111997@gmail.com>
2020-05-23 14:04:17 +0300

    fix change lang

--- components/Select/Select.vue
+++ components/Select/Select.vue
@@ -29,109 +29,109 @@
 </template>
 
 <script>
-  import './index.scss';
+    import './index.scss';
 
-  import SelectDrop from "./components/SelectDrop/SelectDrop";
+    import SelectDrop from "./components/SelectDrop/SelectDrop";
 
-  import objForEach from "../../utils/object-for-each";
-  import destructStateSelect from "../../utils/destruct-state-select";
+    import objForEach from "../../utils/object-for-each";
+    import destructStateSelect from "../../utils/destruct-state-select";
 
-  export default {
-    name: "Select",
-    data () {
-      return {
-        value: {
-          normal: '',
-          checkbox: {},
-          radio: ''
+    export default {
+        name: "Select",
+        data () {
+            return {
+                value: {
+                    normal: '',
+                    checkbox: {},
+                    radio: ''
+                },
+                selected: false,
+                error: false,
+                workCallback: false,
+
+                dropIsOpened: false
+            }
         },
-        selected: false,
-        error: false,
-        workCallback: false,
+        watch: {
+            /// value
+            value (val) {
+                const result = []
+                objForEach(val, (item, key) => {
+                    if (key === 'normal' || key === 'radio') {
+                        item !== '' && result.push(item)
+                    } else {
+                        objForEach(item, (el) => el !== '' ? result.push(el) : undefined)
+                    }
+                })
+                if (this.workCallback || this.$props.initOnStart) {
+                    this.$props.callback({type: 'change', val: result})
+                } else {
+                    !this.workCallback && (this.workCallback = true)
+                }
+            },
+            /*----------------------*/
+        },
+        props: {
+            id: { type: String, default: '', required: true },
+            placeholder: { type: String, default: '' },
+            values: { type: Array, default: () => [] },
+            startValue: { type: Number, default: -1 },
+            moreClass: { type: String, default: '' },
+            closeOnClose: { type: Boolean, default: false },
+            initOnStart: { type: Boolean, default: false },
+            callback: { type: Function, default: () => null }
+        },
+        computed: {
+            /// isOpen - открыт ли селектор
+            isOpen () {
+                const check = `select-id-${this.$props.id}` === this.$root.$store.getters.GET_NOW_OPEN_SELECT
+                if (!check) this.dropIsOpened && (this.dropIsOpened = false)
+                return check
+            },
+            /*----------------------*/
 
-        dropIsOpened: false
-      }
-    },
-    watch: {
-      /// value
-      value (val) {
-        const result = []
-        objForEach(val, (item, key) => {
-          if (key === 'normal' || key === 'radio') {
-            item !== '' && result.push(item)
-          } else {
-            objForEach(item, (el) => el !== '' ? result.push(el) : undefined)
-          }
-        })
-        if (this.workCallback || this.$props.initOnStart) {
-          this.$props.callback({type: 'change', val: result})
-        } else {
-          !this.workCallback && (this.workCallback = true)
+            /// destructValue - получение всех выбранных значений
+            destructValue () {
+                return destructStateSelect(this.value)
+            },
+            /*----------------------*/
+
+            /// hasSelectedValue - есть ли выбранные элементы
+            hasSelectedValue () {
+                return destructStateSelect(this.value) !== ''
+            },
+            /*----------------------*/
+        },
+        methods: {
+            /// getTitle - получение системного сообщения в зависимости от языка
+            getTitle (mes) { return this.$root.$store.getters.RETURN_SYSTEM_MESSAGE(this.$root.$store.getters.GET_SYSTEM_LANG, mes) },
+            /*----------------------*/
+
+            /// openDrop - открыть выбор вариантов
+            openDrop () {
+                if (this.dropIsOpened) {
+                    // Если нужно закрыть
+                    this.$root.$store.dispatch('changeSelectOpen', '')
+                    this.dropIsOpened = false
+                } else {
+                    // Если нужно открыть
+                    this.$root.$store.dispatch('changeSelectOpen', `select-id-${this.$props.id}`)
+                    this.dropIsOpened = true
+                }
+            },
+            /*----------------------*/
+
+            /// callbackForDrop - колбек для отслеживания изменений в dropdown'e
+            callbackForDrop (params) { this.value = params },
+            /*----------------------*/
+        },
+        filters: {
+            /// stateClass
+            stateClass: (error, selected, dropIsOpened, moreClass) => `${moreClass !== '' ? `${moreClass} ` : ''}select${error ? ' error' : (selected ? (dropIsOpened ? ' open val'  : ' val') : (dropIsOpened ? ' open' : ''))}`,
+            /*----------------------*/
+        },
+        components: {
+            SelectDrop
         }
-      },
-      /*----------------------*/
-    },
-    props: {
-      id: { type: String, default: '', required: true },
-      placeholder: { type: String, default: '' },
-      values: { type: Array, default: () => [] },
-      startValue: { type: Number, default: -1 },
-      moreClass: { type: String, default: '' },
-      closeOnClose: { type: Boolean, default: false },
-      initOnStart: { type: Boolean, default: false },
-      callback: { type: Function, default: () => null }
-    },
-    computed: {
-      /// isOpen - открыт ли селектор
-      isOpen () {
-        const check = `select-id-${this.$props.id}` === this.$root.$store.getters.GET_NOW_OPEN_SELECT
-        if (!check) this.dropIsOpened && (this.dropIsOpened = false)
-        return check
-      },
-      /*----------------------*/
-
-      /// destructValue - получение всех выбранных значений
-      destructValue () {
-        return destructStateSelect(this.value)
-      },
-      /*----------------------*/
-
-      /// hasSelectedValue - есть ли выбранные элементы
-      hasSelectedValue () {
-        return destructStateSelect(this.value) !== ''
-      },
-      /*----------------------*/
-    },
-    methods: {
-      /// getTitle - получение системного сообщения в зависимости от языка
-      getTitle (mes) { return this.$root.$store.getters.RETURN_SYSTEM_MESSAGE(this.$root.$store.getters.GET_SYSTEM_LANG, mes) },
-      /*----------------------*/
-
-      /// openDrop - открыть выбор вариантов
-      openDrop () {
-        if (this.dropIsOpened) {
-          // Если нужно закрыть
-          this.$root.$store.dispatch('changeSelectOpen', '')
-          this.dropIsOpened = false
-        } else {
-          // Если нужно открыть
-          this.$root.$store.dispatch('changeSelectOpen', `select-id-${this.$props.id}`)
-          this.dropIsOpened = true
-        }
-      },
-      /*----------------------*/
-
-      /// callbackForDrop - колбек для отслеживания изменений в dropdown'e
-      callbackForDrop (params) { this.value = params },
-      /*----------------------*/
-    },
-    filters: {
-      /// stateClass
-      stateClass: (error, selected, dropIsOpened, moreClass) => `${moreClass !== '' ? `${moreClass} ` : ''}select${error ? ' error' : (selected ? (dropIsOpened ? ' open val'  : ' val') : (dropIsOpened ? ' open' : ''))}`,
-      /*----------------------*/
-    },
-    components: {
-      SelectDrop
     }
-  }
 </script>
--- components/Select/components/SelectDrop/SelectDrop.vue
+++ components/Select/components/SelectDrop/SelectDrop.vue
@@ -36,94 +36,105 @@
 </template>
 
 <script>
-  import './index.scss';
+    import './index.scss';
 
-  import RadioInput from "../../../RadioInput/RadioInput";
+    import RadioInput from "../../../RadioInput/RadioInput";
 
-  export default {
-    name: "SelectDrop",
-    data () {
-      return {
-        value: {
-          normal: '',
-          checkbox: {},
-          radio: ''
+    export default {
+        name: "SelectDrop",
+        data () {
+            return {
+                value: {
+                    normal: '',
+                    checkbox: {},
+                    radio: ''
+                },
+            }
         },
-      }
-    },
-    props: {
-      open: { type: Boolean, default: false }, // открыт ли dropdown
-      values: { type: Array, default: () => [] }, // значения
-      startValue: { type: Number, default: -1 },
-      closeOnClose: { type: Boolean, default: false },
-      callback: { type: Function, default: () => null }
-    },
-    methods: {
-      /// selectOption - метод изменения выбранных данных
-      selectOption (type = '', value = '', name = '') {
-        let cloneValue = {...this.value}
-        if (type !== '' && value !== '' && name !== '') {
-          switch (type) {
-            case "normal": {
-              if (cloneValue['normal'] === value)
-                cloneValue['normal'] = ''
-              else
-                cloneValue['normal'] = value
-            } break;
-            case "radio": {
-              if (cloneValue['radio'] === value)
-                cloneValue['radio'] = ''
-              else
-                cloneValue['radio'] = value
-            } break;
-            default: {
-              let cloneCheckboxValue = {...cloneValue['checkbox']}
-              if (cloneCheckboxValue[name] === undefined) {
-                cloneCheckboxValue[name] = value
-              } else {
-                delete cloneCheckboxValue[name]
-              }
-              cloneValue['checkbox'] = cloneCheckboxValue
-            } break;
-          }
-          this.value = cloneValue
-          this.$props.callback(cloneValue)
-          if (this.$props.closeOnClose) {
-            this.$root.$store.dispatch('changeSelectOpen', '')
-          }
-        }
-      },
-      /*----------------------*/
-    },
-    filters: {
-      /// classForNormalOption - установка списка классов для обычного отображения
-      classForNormalOption: (lastIndex = false, active = false) => `select-normal${lastIndex ? ' last' : ''}${active ? ' active' : ''}`,
-      /*----------------------*/
+        props: {
+            open: { type: Boolean, default: false }, // открыт ли dropdown
+            values: { type: Array, default: () => [] }, // значения
+            startValue: { type: Number, default: -1 },
+            closeOnClose: { type: Boolean, default: false },
+            callback: { type: Function, default: () => null }
+        },
+        watch: {
+            /// startValue
+            values () { this.updateStartValue() },
+            /*----------------------*/
+        },
+        methods: {
+            /// selectOption - метод изменения выбранных данных
+            selectOption (type = '', value = '', name = '') {
+                let cloneValue = {...this.value}
+                if (type !== '' && value !== '' && name !== '') {
+                    switch (type) {
+                        case "normal": {
+                            if (cloneValue['normal'] === value)
+                                cloneValue['normal'] = ''
+                            else
+                                cloneValue['normal'] = value
+                        } break;
+                        case "radio": {
+                            if (cloneValue['radio'] === value)
+                                cloneValue['radio'] = ''
+                            else
+                                cloneValue['radio'] = value
+                        } break;
+                        default: {
+                            let cloneCheckboxValue = {...cloneValue['checkbox']}
+                            if (cloneCheckboxValue[name] === undefined) {
+                                cloneCheckboxValue[name] = value
+                            } else {
+                                delete cloneCheckboxValue[name]
+                            }
+                            cloneValue['checkbox'] = cloneCheckboxValue
+                        } break;
+                    }
+                    this.value = cloneValue
+                    this.$props.callback(cloneValue)
+                    if (this.$props.closeOnClose) {
+                        this.$root.$store.dispatch('changeSelectOpen', '')
+                    }
+                }
+            },
+            /*----------------------*/
 
-      /// classForOtherTypeOption - установка списка классов для остальных типов отображения
-      classForOtherTypeOption: (last = false, classList = '') => `${classList}${last ? ' last' : ''}`,
-      /*----------------------*/
-    },
-    components: {
-      RadioInput
-    },
-    mounted () {
-      const {startValue, values, callback} = this.$props
-      if (startValue !== -1) {
-        if (values[startValue] !== undefined) {
-          const startType = values[startValue].type
-          switch (startType) {
-            case "normal": this.value['normal'] = values[startValue].value; break;
-            case "radio": this.value['radio'] = values[startValue].value; break;
-            default: {
-              let template = {}
-              template[values[startValue].name] = values[startValue].value
-              this.value['checkbox'] = {...template}
-            } break;
-          }
-          callback(this.value)
+            /// updateStartValue - обновление стартового значения
+            updateStartValue () {
+                const {startValue, values, callback} = this.$props
+                if (startValue !== -1) {
+                    if (values[startValue] !== undefined) {
+                        const startType = values[startValue].type
+                        switch (startType) {
+                            case "normal": this.value['normal'] = values[startValue].value; break;
+                            case "radio": this.value['radio'] = values[startValue].value; break;
+                            default: {
+                                let template = {}
+                                template[values[startValue].name] = values[startValue].value
+                                this.value['checkbox'] = {...template}
+                            } break;
+                        }
+                        callback(this.value)
+                    }
+                }
+            },
+            /*----------------------*/
+        },
+        filters: {
+            /// classForNormalOption - установка списка классов для обычного отображения
+            classForNormalOption: (lastIndex = false, active = false) => `select-normal${lastIndex ? ' last' : ''}${active ? ' active' : ''}`,
+            /*----------------------*/
+
+            /// classForOtherTypeOption - установка списка классов для остальных типов отображения
+            classForOtherTypeOption: (last = false, classList = '') => `${classList}${last ? ' last' : ''}`,
+            /*----------------------*/
+        },
+        components: {
+            RadioInput
+        },
+        mounted () {
+            this.updateStartValue()
         }
-      }
     }
-  }
 </script>
